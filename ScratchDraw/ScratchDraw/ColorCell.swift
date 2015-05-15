28b4cdc3f22167d3ddb87cb9eba059e98b04bdb0
Reed Carson <rpcarson27@gmail.com>
2015-05-15 15:40:10 -0400

    friday 3:40

---
+++ ScratchDraw/ScratchDraw/ColorCell.swift
@@ -0,0 +1,21 @@
+//
+//  colorCell.swift
+//  ScratchDraw
+//
+//  Created by Reed Carson on 5/15/15.
+//  Copyright (c) 2015 Reed Carson. All rights reserved.
+//
+
+import UIKit
+
+class ColorCell: UICollectionViewCell {
+    
+    
+
+    
+    
+    
+    
+    
+    
+}
--- ScratchDraw/ScratchDraw/ViewController.swift
+++ ScratchDraw/ScratchDraw/ViewController.swift
@@ -8,33 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var fillColors: [UIColor] = [
+    UIColor.cyanColor(),
+    UIColor.magentaColor(),
+    UIColor.yellowColor(),
+    UIColor.blackColor(),
+    UIColor.redColor(),
+    UIColor.greenColor(),
+    UIColor.blueColor()
+]
+
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     
-    @IBAction func changeColor(sender: UIButton) {
-        
-        if let color = sender.backgroundColor {
-        scratchPad.currentColor = color
-        
-        }
-    }
+    @IBOutlet weak var viewBottomConstraints: NSLayoutConstraint!
+    
+    @IBOutlet weak var fillColorCollectionView: UICollectionView!
+
+//    @IBAction func changeColor(sender: UIButton) {
+//        
+//        if let color = sender.backgroundColor {
+//            scratchPad.currentColor = color
+//            
+//        }
+//    }
     
     @IBAction func changeStroke(sender: UIButton) {
         
         
     }
-    
-    
-    
+
     @IBOutlet weak var strokeVisual: UIButton!
     
     @IBOutlet weak var clearVisual: UIButton!
     
     @IBAction func clearLines(sender: AnyObject) {
-   
-    scratchPad.scratches = []
-        scratchPad.setNeedsDisplay()    
-    
+        
+        scratchPad.scratches = []
+        scratchPad.setNeedsDisplay()
+        
     }
     
     
@@ -47,13 +60,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        view.addSubview(scratchPad)
-//        scratchPad.frame = view.frame
-//        scratchPad.backgroundColor = UIColor.redColor()
-        clearVisual.layer.cornerRadius = 10
-        clearVisual.alpha = 0.5
-        strokeVisual.layer.cornerRadius = 10
+        fillColorCollectionView.dataSource = self
+        fillColorCollectionView.delegate = self
+        
+        viewBottomConstraints.constant = -300
+        
     }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return fillColors.count
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("colorCell", forIndexPath: indexPath) as! ColorCell
+        
+       cell.backgroundColor = fillColors[indexPath.item]
+        
+        return cell
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        let cell = collectionView.cellForItemAtIndexPath(indexPath) as! ColorCell
+        
+        if let color = cell.backgroundColor {
+           
+            scratchPad.currentColor = color
+            
+        }
+        
+        
+    }
+    
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         
@@ -73,14 +116,25 @@ class ViewController: UIViewController {
         if let touch = touches.first as? UITouch {
             
             let location = touch.locationInView(scratchPad)
-//            scratchPad.updateCurrentScratchWithLastPoint(location)
             scratchPad.addPointToCurrentScratch(location)
             
         }
         
     }
     
-
+    @IBAction func collapse(sender: UIButton) {
+        
+        viewBottomConstraints.constant = (viewBottomConstraints.constant == 0) ? -300 : 0
+        
+        
+        
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+        })
+        
+    
+    
+    }
+    
     
 }
 
